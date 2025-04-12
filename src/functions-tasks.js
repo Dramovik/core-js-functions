aa8c933838e84e4a128ffe6026731400b0cdfd05
Dramovik <dramovik1@gmail.com>
2025-04-13 02:18:21 +0300

    feat: done getPolynom

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -92,8 +92,13 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...coefficients) {
+  return function (x) {
+    return coefficients.reduce((sum, coeff, index) => {
+      const power = coefficients.length - index - 1;
+      return sum + coeff * x ** power;
+    }, 0);
+  };
 }
 
 /**
